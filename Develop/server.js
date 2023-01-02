dc798d74121495a833fc4f7ddd99f1e7a49e25b9
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-02 12:42:39 -0500

    edited server file and added clog file

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('.routes/index.js');
+const api = require('./public/assets/js/index.js');
+const savedNotes = require('./db/db.json');
 
 // Defines port
 const PORT = process.env.PORT || 3001;
@@ -13,20 +14,21 @@ app.use(clog);
 
 app.use('/api', api);
 
-// GET Route for Landing page
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for Notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard Route for 404 Page
+// GET Route for Landing page
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/404.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+// GET for notes api
+app.get('/api/notes', (req, res) => res.json(savedNotes));
+
+// POST for notes api
+app.post('/api/notes', (req, res) => res.json(savedNotes));
 
 // Local Server
 app.listen(PORT, () =>
