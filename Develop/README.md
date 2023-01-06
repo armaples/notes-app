089486f951dccf12b87154faa59e79bd1770c465
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-05 19:02:03 -0500

    Finished basic requirements, added README file

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,48 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+This project ensures the functionality of provided starter code for a note taking app. 
+
+## Table of Contents
+
+- [Application Preview](#application-preview)
+- [Important Links](#important-links)
+- [Credits](#credits)
+- [License](#license)
+    - [MIT License](#mit-license)
+
+## Application Preview
+
+
+## Important Links
+
+- [Github Repository]()
+- [Deployed Application]()
+
+## Credits
+- [Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide) 
+- [Choose A License](https://choosealicense.com/licenses/mit/)
+
+## License
+
+### MIT License
+
+Copyright (copy) 2022 Note Taker - Alyssa Maples
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./public/assets/js/index.js');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 const savedNotes = require('./db/db.json');
 
 // Defines port
@@ -10,25 +10,103 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Imports middleware from cLog
-app.use(clog);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/api', api);
+app.use(express.static('public'));
 
 // GET Route for Notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET Route for retrieving saved notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(JSON.parse(data));
+            console.info(JSON.parse(data));
+        };
+});
+});
+
+// GET Route for getting a specific note
+app.get('/api/notes/:id', (req, res) => {
+    if (req.params.id) {
+        console.info(`${req.method} request received to get specific note.`);
+        const noteId = req.params.id;
+        for (let i = 0; i < savedNotes.length; i++) {
+            const activeNote = savedNotes[i];
+            if (activeNote.id === noteId) {
+                res.json(activeNote);
+                return;
+            }
+        }
+        res.status(404).send('Note not found');
+    }
+})
+
+// POST Route for saving new note
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to save a note.`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4), 
+                    (writeErr) =>
+                    writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully saved to notes.')
+                );
+            }
+        })
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error saving note.');
+    }
+});
+
+// DELETE Route for if you want to delete a note
+app.delete('/api/notes/:id', (req, res) => {
+    res.send("DELETE Request Called");
+
+    //recieve query parameter that contains id of note to delete
+
+    // read all notes
+    // remove note with specific id
+    // rewrite db.json file
+})
+
 // GET Route for Landing page
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-// GET for notes api
-app.get('/api/notes', (req, res) => res.json(savedNotes));
-
-// POST for notes api
-app.post('/api/notes', (req, res) => res.json(savedNotes));
 
 // Local Server
 app.listen(PORT, () =>
